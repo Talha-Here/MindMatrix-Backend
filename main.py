0a853eede536871e85f8e98fc5191e6d18105afd
TalhaHKhan <talhakhan2016thk@gmail.com>
2024-12-10 06:10:25 +0500

    update

--- main.py
+++ main.py
@@ -151,4 +151,4 @@ async def fetch_news_route(request: NewsRequest):
 # Run the app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port= 8000, reload = True )
